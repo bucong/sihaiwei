1870341d2c4b043a4fdadef4086fe8c4e31f4fd8
bucong <531053266@qq.com>
2018-11-06 19:13:10 +0800

    update

--- src/util/projectData.js
+++ src/util/projectData.js
@@ -7,6 +7,7 @@ const IMG = 'http://47.100.51.191:3000/public/img/';
 const thridLoginUrl = {
   wxEncode: 'http%3a%2f%2f47.100.51.191%2fsihaiwei%2f',
 
+  // qqEncode: 'http%3a%2f%2flocalhost%3a8088%2fsihaiwei%2f',
   qqEncode: 'http%3a%2f%2f47.100.51.191%2fsihaiwei%2f',
 
   qq: 'http://share.zrpic.com/jnwtv-live-cartoon-h5/test.html'
