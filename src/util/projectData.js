cbc17d46febcf0cbb07ba93ef6d38581dd54665c
bucong <531053266@qq.com>
2018-11-06 09:56:15 +0800

    修改服务器地址

--- src/util/projectData.js
+++ src/util/projectData.js
@@ -1,16 +1,15 @@
 //服务器地址
-const URL = 'http://localhost:3000/api/';
+const URL = 'http://47.100.51.191:3000/api/';
 //服务器图片地址
-const IMG = 'http://localhost:3000/public/img/';
+const IMG = 'http://47.100.51.191:3000/public/img/';
 
 //三方登录回调地址
 const thridLoginUrl = {
-  wxEncode: 'http%3a%2f%2fshare.zrpic.com%2f',
+  wxEncode: 'http%3a%2f%2f47.100.51.191%3a3000%2f',
 
-  qqEncode: 'http%3a%2f%2foffice.zrpic.com%2f',
+  qqEncode: 'http%3a%2f%2f47.100.51.191%3a3000%2f',
 
   qq: 'http://share.zrpic.com/jnwtv-live-cartoon-h5/test.html'
-  // qq: 'http://share.zrpic.com/jnwtv-live-cartoon-h5/'
 };
 
 export { URL, IMG, thridLoginUrl };
