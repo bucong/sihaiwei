736820b18d2659c28acc9779714198d06823b093
bucong <531053266@qq.com>
2018-11-06 16:17:05 +0800

    修改微信授权回调

--- src/util/thirdLogin.js
+++ src/util/thirdLogin.js
@@ -5,7 +5,7 @@ export function thirdLogin(){
     //微信服务号，删除点击头像退出登录，添加数据埋点
     // window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxeb904482e11189cf&redirect_uri="+thridLoginUrl.wxEncode+"&response_type=code&scope=snsapi_userinfo&state=wx#wechat_redirect";
     //微信测试通道
-    window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxeb904482e11189cf&redirect_uri=http%3a%2f%2fshare.zrpic.com%2fjnwtv-live-cartoon-h5%2ftest.html&response_type=code&scope=snsapi_userinfo&state="+thridLoginUrl.qqEncode+"%2f#wechat_redirect";
+    window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxeb904482e11189cf&redirect_uri=http%3a%2f%2fshare.zrpic.com%2fjnwtv-live-cartoon-h5%2ftest.html&response_type=code&scope=snsapi_userinfo&state="+thridLoginUrl.qqEncode+"#wechat_redirect";
   }else{
     // 其他浏览器使用QQ
     // window.location.href="https://graph.qq.com/oauth2.0/authorize?client_id=101479867&redirect_uri="+thridLoginUrl.qqEncode+"&response_type=code&scope=get_user_info&state=qq&display=mobile";
