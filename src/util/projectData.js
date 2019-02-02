c8c46272882e4c090466dbe0dab7cf1eaa199904
bucong <531053266@qq.com>
2019-02-02 17:29:40 +0800

    QQ回调

--- src/util/projectData.js
+++ src/util/projectData.js
@@ -7,11 +7,11 @@ const URL = 'http://47.100.51.191:3000/api/';
 
 //三方登录回调地址
 const thridLoginUrl = {
-  wxEncode: 'http%3a%2f%2f47.100.51.191%2f',
+  wxEncode: 'http%3a%2f%2f47.100.51.191%2fsihaiwei%2f',
 
-  // qqEncode: 'http%3a%2f%2flocalhost%3a8088%2f',
-  // qqEncode: 'http%3a%2f%2f192.168.0.118%3a8088%2f',
-  qqEncode: 'http%3a%2f%2f47.100.51.191%2f',
+  // qqEncode: 'http%3a%2f%2flocalhost%3a8088%2fsihaiwei%2f',
+  // qqEncode: 'http%3a%2f%2f192.168.0.118%3a8088%2fsihaiwei%2f',
+  qqEncode: 'http%3a%2f%2f47.100.51.191%2fsihaiwei%2f',
 
   qq: 'http://share.zrpic.com/jnwtv-live-cartoon-h5/test.html'
 };
